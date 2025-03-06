a7d00cd7df6fd583c6fd9f58ff51db134a4942b0
daonguyen2410 <dnguyen0967532677@gmail.com>
2025-03-06 14:32:50 +0700

    CRUD product

--- resource/back-end/src/routers/index.js
+++ resource/back-end/src/routers/index.js
@@ -1,15 +1,16 @@
+
 const express = require('express');
 const authRouters = require('./authRouters')
 const managerRouters = require('./managerRouters')
 const shopRouters = require('./shopRouters')
 const customerRouters = require('./customerRouters')
 const categoryController = require('./categoryRouters')
-
+const productRouters = require('./productRouters')
 const router = express.Router();
 
 router.use('/auth', authRouters);
 router.use('/manager', managerRouters);
-
+router.use('/product', productRouters);
 router.use('/shop', shopRouters);
 router.use('/customer', customerRouters);
 router.use('/category', categoryController);
